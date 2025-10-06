059a630da4d45daa85356afa14d3d14655c237c9
Samue45 <samuelayllonsevilla86@gmail.com>
2025-10-06 10:16:22 +0200

    V2-Changing structure

--- Project/src/app/app.ts
+++ Project/src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductComponent } from './components/product-component/product-component';
+import { ProductI } from './components/interfaces/product-i';
+import { ProductService } from './service/product-service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,47 @@ import { ProductComponent } from './components/product-component/product-compone
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Project');
+   public arrayProductos : ProductI[];
+
+
+  @Output() product = new EventEmitter<ProductI>();
+
+  constructor(private productoService: ProductService) { 
+    this.arrayProductos = this.productoService.getAllProducts();
+  }
+
+  public addProduct(id: string, name: string, price: string) {
+    try {
+      this.productoService.addProduct(Number(id), name, Number(price));
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Error adding product:', error.message);
+      } else {
+        console.error('Unexpected error', error);
+      }
+    }
+  }
+  public removeProduct(id: String) {
+    try {
+      this.productoService.removeProduct(Number(id));
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Error removing product:', error.message);
+      } else {
+        console.error('Unexpected error', error);
+      }
+    } 
+  }
+
+  public updateProduct(id: number, name: string, price: number) {
+    try {
+      this.productoService.updateProduct(id,name,price);
+    }catch(error){
+      if(error instanceof Error){
+        console.error('Error removing product:', error.message);
+      } else {
+        console.error('Unexpected error', error);
+      }
+    }
+  }
 }
--- Project/src/app/components/product-component/product-component.ts
+++ Project/src/app/components/product-component/product-component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { ProductService } from '../../service/product-service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductI } from '../interfaces/product-i';
 import { FormsModule } from '@angular/forms';
@@ -12,46 +11,9 @@ import { FormsModule } from '@angular/forms';
 })
 export class ProductComponent {
 
-  public arrayProductos : ProductI[];
+  @Input() arrayProductos: ProductI[] = [];
+  @Output() product = new EventEmitter<ProductI>();
 
-  constructor(private productoService: ProductService) { 
-    this.arrayProductos = this.productoService.getAllProducts();
-  }
 
-  public addProduct(id: string, name: string, price: string) {
-    try {
-      this.productoService.addProduct(Number(id), name, Number(price));
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Error adding product:', error.message);
-      } else {
-        console.error('Unexpected error', error);
-      }
-    }
-  }
-  public removeProduct(id: String) {
-    try {
-      this.productoService.removeProduct(Number(id));
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Error removing product:', error.message);
-      } else {
-        console.error('Unexpected error', error);
-      }
-    } 
-  }
-
-  public updateProduct(id: number, name: string, price: number) {
-    try {
-      this.productoService.updateProduct(id,name,price);
-    }catch(error){
-      if(error instanceof Error){
-        console.error('Error removing product:', error.message);
-      } else {
-        console.error('Unexpected error', error);
-      }
-    }
-  }
-  
 
 }
