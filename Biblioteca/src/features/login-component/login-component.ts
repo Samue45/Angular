5cfff6bff37de221e5a29e2e50e9d55c9a6bf41f
Samue45 <samuelayllonsevilla86@gmail.com>
2025-11-12 12:52:50 +0100

    Creando el formulario

--- Biblioteca/src/features/login-component/login-component.ts
+++ Biblioteca/src/features/login-component/login-component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
 export class LoginComponent {
+    miFormulario: FormGroup;
+
+    constructor(){
+      this.miFormulario = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('',[Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{6,}$')
+        ])
+      });
+    }
+
+    send() {
+      if(this.miFormulario.valid){
+        //Llevar a la próxima ruta
+      }else{
+
+      }
+    }
 
 }
