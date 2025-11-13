8d755b590bad80689ba79e4b18641c2d489a3434
Samu-Mind <samuelayllonsevilla86@gmail.com>
2025-11-13 20:04:38 +0100

    Componente home terminado

--- Biblioteca/src/app/app.routes.ts
+++ Biblioteca/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../features/home-component/home-component';
 import { LoginComponent } from '../features/login-component/login-component';
 import { authGuard } from './guards/auth-guard';
+import { BibliotecaComponent } from '../features/biblioteca-component/biblioteca-component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // redirección
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate:[authGuard] },
+  { path: 'biblioteca', component: BibliotecaComponent, canActivate:[authGuard] },
   { path: '**', redirectTo: '/home' } // ruta comodín
 ];
 
--- Biblioteca/src/features/home-component/home-component.ts
+++ Biblioteca/src/features/home-component/home-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private router: Router){}
+
+  gestionarLibros(){
+    this.router.navigate(['biblioteca']);
+  }
 }
