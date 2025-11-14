671199cec4a197fc74afeac914d59296c1633036
Samu-Mind <samuelayllonsevilla86@gmail.com>
2025-11-14 07:46:13 +0100

    Componente para filtrar funcional

--- Biblioteca/src/features/filtro-lateral-component/filtro-lateral-component.ts
+++ Biblioteca/src/features/filtro-lateral-component/filtro-lateral-component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { LibrosService } from '../../core/libros-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filtro-lateral-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './filtro-lateral-component.html',
   styleUrl: './filtro-lateral-component.scss',
 })
 export class FiltroLateralComponent {
+
+  filtroTitulo: string = '';
+
+  constructor(private librosService: LibrosService) { }
+
+
+  filtrarLibros(): void {
+    this.librosService.getLibrosByName$(this.filtroTitulo);
+  }
 
 }
